e368870a1a38f06dfe70e1a81641e0aa3fc3cca0
MrAdib <Mr.Javad.Adib@gmail.com>
2022-09-10 02:22:14 +0430

    round rate to 3 decimal

--- ui/src/organisms/Widget.jsx
+++ ui/src/organisms/Widget.jsx
@@ -32,26 +32,34 @@ export default function Widget(props) {
 
     const itemsEl = Object.entries(myRates).map((item, index) => {
       const currency = item[0];
+      const exchangeRate = Math.round((item[1] + Number.EPSILON) * 1000) / 1000;
+
       if (currency === baseCurrency) {
-        return "";
+        return;
       }
       return (
         <li key={currency} className="flex px-2 hover:bg-black/10 transition" title={currency}>
           <div className="grow font-light">{apiData.symbols[currency]}</div>
-          <div className="">{item[1]}</div>
+          <div className="">{exchangeRate}</div>
         </li>
       );
     });
 
     return (
-      <section className="widget max-w-sm m-2 border rounded border-lime-600">
+      <section className="widget max-w-sm m-2 border rounded leading-7 select-none border-lime-600">
         <header className="bg-lime-600 text-white px-2 leading-10">
-          <h2>{apiData.symbols[baseCurrency]} Exchange Rate</h2>
+          <h2>{apiData.symbols[baseCurrency]} Exchange Rates</h2>
         </header>
-        <ul className=" rounded overflow-hidden leading-7 select-none transition hover:bg-slate-50">
+        <ul className=" rounded overflow-hidden transition hover:bg-slate-50">
           <li className="px-2 text-right font-semibold">1 {baseCurrency} =</li>
           {itemsEl}
         </ul>
+        <footer
+          className="px-2 rounded text-right leading-10 bg-gray-100 text-gray-500"
+          title={"Expired at " + apiData.dateUpdate}
+        >
+          Last Update {apiData.dateUpdate}
+        </footer>
       </section>
     );
   }
