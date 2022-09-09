2cc81f6881c7cf7f56d7f0166d5fc546f2a7fd30
MrAdib <Mr.Javad.Adib@gmail.com>
2022-09-10 02:49:17 +0430

    handle loading error

--- ui/src/organisms/Widget.jsx
+++ ui/src/organisms/Widget.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { SampleData } from "../pages/SampleData.js";
 
 export default function Widget(props) {
   const [error, setError] = useState(null);
@@ -13,54 +14,62 @@ export default function Widget(props) {
         (result) => {
           setIsLoaded(true);
           setApiData(result);
+          console.log(result);
         },
         (error) => {
           setIsLoaded(true);
+          setApiData(SampleData);
           setError(error);
         }
       );
   }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else if (apiData.status !== 200) {
-    return <div>Error: {apiData.error}</div>;
-  } else {
-    const myRates = apiData.latest.rates;
+  if (!isLoaded) {
+    return <div className="py-2 px-4 leading-10">Loading...</div>;
+  }
 
-    const itemsEl = Object.entries(myRates).map((item, index) => {
-      const currency = item[0];
-      const exchangeRate = Math.round((item[1] + Number.EPSILON) * 1000) / 1000;
+  const myRates = apiData.latest.rates;
 
-      if (currency === baseCurrency) {
-        return;
-      }
-      return (
-        <li key={currency} className="flex px-2 hover:bg-black/10 transition" title={currency}>
-          <div className="grow font-light">{apiData.symbols[currency]}</div>
-          <div className="">{exchangeRate}</div>
-        </li>
-      );
-    });
-
+  const itemsEl = Object.entries(myRates).map((item, index) => {
+    const currency = item[0];
+    const exchangeRate = Math.round((item[1] + Number.EPSILON) * 1000) / 1000;
+    if (currency === baseCurrency) {
+      return;
+    }
     return (
-      <section className="widget max-w-sm m-2 border rounded leading-7 select-none border-lime-600">
-        <header className="bg-lime-600 text-white px-2 leading-10">
-          <h2>{apiData.symbols[baseCurrency]} Exchange Rates</h2>
-        </header>
-        <ul className=" rounded overflow-hidden transition hover:bg-slate-50">
-          <li className="px-2 text-right font-semibold">1 {baseCurrency} =</li>
-          {itemsEl}
-        </ul>
-        <footer
-          className="px-2 rounded text-right leading-10 bg-gray-100 text-gray-500"
-          title={"Expired at " + apiData.dateUpdate}
-        >
-          Last Update {apiData.dateUpdate}
-        </footer>
-      </section>
+      <li key={currency} className="flex px-2 hover:bg-black/10 transition" title={currency}>
+        <div className="grow font-light">{apiData.symbols[currency]}</div>
+        <div className="">{exchangeRate}</div>
+      </li>
+    );
+  });
+
+  let footerText = "Update " + apiData.dateUpdate;
+  let footerTitle = footerText + " - Expired at " + apiData.dateUpdate;
+
+  if (error || apiData.status !== 200) {
+    footerText = (
+      <span className="text-red-400">
+        {footerText} - {error ? error.message : apiData.error}
+      </span>
     );
   }
+
+  return (
+    <section className="widget max-w-sm m-2 border rounded leading-7 select-none border-lime-600">
+      <header className="bg-lime-600 text-white px-2 leading-10">
+        <h2>{apiData.symbols[baseCurrency]} Exchange Rates</h2>
+      </header>
+      <ul className=" rounded overflow-hidden transition hover:bg-slate-50">
+        <li className="px-2 text-right font-semibold">1 {baseCurrency} =</li>
+        {itemsEl}
+      </ul>
+      <footer
+        className="px-2 rounded truncate leading-10 text-xs bg-gray-100 text-gray-500"
+        title={footerTitle}
+      >
+        {footerText}
+      </footer>
+    </section>
+  );
 }
