0385b6f9465ff46eab079092df3210f98f28a886
MrAdib <Mr.Javad.Adib@gmail.com>
2022-09-09 16:03:14 +0430

    redirect root module to api

--- application/app/controllers/IndexController.php
+++ application/app/controllers/IndexController.php
@@ -10,9 +10,7 @@ class IndexController extends ControllerBase
         // disable view
         $this->view->disable();
 
-        // $mySymbols = new \Symbols();
-        // $famous = $mySymbols->getFamous();
-        // echo json_encode($famous);
-        echo json_encode([1, 2, 3,]);
+        // redirect root to api
+        $this->response->redirect('/api');
     }
 }
