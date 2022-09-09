077c2afd9f561a7f9a2c04144613278e7a5bdf8e
MrAdib <Mr.Javad.Adib@gmail.com>
2022-09-09 21:54:21 +0430

    add call to action line

--- ui/src/templates/ExchangeTemplate.js
+++ ui/src/templates/ExchangeTemplate.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Header } from "../organisms/Header";
 import { Hero } from "../organisms/Hero";
+import { CallToAction } from "../organisms/CallToAction";
 
 function ExchangeTemplate(props) {
-  const pageStyle = "h-screen overflow-hidden";
+  const pageStyle = "";
 
   return (
     <div className={pageStyle}>
       <Header />
       <main>
         <Hero />
+        <CallToAction />
       </main>
     </div>
   );
