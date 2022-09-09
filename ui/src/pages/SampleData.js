9b00bef066c0e4f5ed40e16281614e9aa3d23b85
MrAdib <Mr.Javad.Adib@gmail.com>
2022-09-09 20:01:45 +0430

    add sample data of backend

---
+++ ui/src/pages/SampleData.js
@@ -0,0 +1,46 @@
+export const SampleData = {
+  okay: true,
+  status: 200,
+  error: false,
+  ttl: 3600,
+  dateUpdate: "2022-09-09 14:56:20",
+  dateExpire: "2022-09-09 15:56:20",
+  latest: {
+    success: true,
+    timestamp: 1662735364,
+    base: "USD",
+    date: "2022-09-09",
+    rates: {
+      USD: 1,
+      EUR: 0.995675,
+      GBP: 0.863045,
+      CHF: 0.96105,
+      CAD: 1.30451,
+      AUD: 1.462694,
+      JPY: 142.437498,
+      CNY: 6.925201,
+      RUB: 60.598362,
+      IRR: 42299.999889,
+      AED: 3.673033,
+      TRY: 18.237992,
+      IQD: 1460,
+      INR: 79.60025
+    }
+  },
+  symbols: {
+    USD: "United States Dollar",
+    EUR: "Euro",
+    GBP: "British Pound Sterling",
+    CHF: "Swiss Franc",
+    CAD: "Canadian Dollar",
+    AUD: "Australian Dollar",
+    JPY: "Japanese Yen",
+    CNY: "Chinese Yuan",
+    RUB: "Russian Ruble",
+    IRR: "Iranian Rial",
+    AED: "United Arab Emirates Dirham",
+    TRY: "Turkish Lira",
+    IQD: "Iraqi Dinar",
+    INR: "Indian Rupee"
+  }
+};
