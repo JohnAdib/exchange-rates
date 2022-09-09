51a4330fa71bfa6e3d85e9dda396b18727d98854
MrAdib <Mr.Javad.Adib@gmail.com>
2022-09-09 20:39:22 +0430

    add header

--- ui/src/templates/ExchangeTemplate.js
+++ ui/src/templates/ExchangeTemplate.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Header } from "../organisms/Header";
 import BoardHeader from "../organisms/BoardHeader";
 import BoardLists from "../organisms/BoardLists";
 
@@ -7,6 +8,7 @@ function ExchangeTemplate(props) {
 
   return (
     <div className={pageStyle}>
+      <Header />
       <BoardHeader />
       <BoardLists data={props.data} />
     </div>
